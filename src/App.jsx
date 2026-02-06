27f63511e39cbcf1a0f6f240e76bbcd06410660b
Nick Belov <ogrillazz@gmail.com>
2026-02-06 13:25:58 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+          <header className="main-header">
+
+          <div className="logo"><span>.</span>ne</div>
+
+          <a href="#" className="add-new">Add New</a>
+
+          <div className="head-text">
+              <h1><span>E</span>vents for<br/>the week</h1>
+          </div>
+
+          {/*.card*8>img[src="src/assets/images/$.jpg"][alt="Alt"]+h3>lorem3^p>lorem10*/}
+          {/*    <div className="card"><img src="src/assets/images/1.jpg" alt="Alt"/>*/}
+          {/*        <h3>Lorem ipsum dolor.</h3>*/}
+          {/*        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, excepturi?</p>*/}
+          {/*    </div>*/}
+          {/*    <div className="card"><img src="src/assets/images/2.jpg" alt="Alt"/>*/}
+          {/*        <h3>Aut ipsum, placeat.</h3>*/}
+          {/*        <p>Eius ipsam iure odio quasi quisquam recusandae sed totam voluptas.</p>*/}
+          {/*    </div>*/}
+          {/*    <div className="card"><img src="src/assets/images/3.jpg" alt="Alt"/>*/}
+          {/*        <h3>Impedit, ipsum porro.</h3>*/}
+          {/*        <p>At debitis dolor eius esse, necessitatibus possimus quidem? Assumenda, dolores.</p>*/}
+          {/*    </div>*/}
+          {/*    <div className="card"><img src="src/assets/images/4.jpg" alt="Alt"/>*/}
+          {/*        <h3>Aperiam et, necessitatibus?</h3>*/}
+          {/*        <p>Accusantium animi aperiam, architecto deleniti dignissimos dolor quam quasi sed!</p>*/}
+          {/*    </div>*/}
+          {/*    <div className="card"><img src="src/assets/images/5.jpg" alt="Alt"/>*/}
+          {/*        <h3>Labore tempora, totam.</h3>*/}
+          {/*        <p>Dolorem eum impedit natus, officia pariatur quo rerum sed sit?</p>*/}
+          {/*    </div>*/}
+          {/*    <div className="card"><img src="src/assets/images/6.jpg" alt="Alt"/>*/}
+          {/*        <h3>Molestias obcaecati, reiciendis!</h3>*/}
+          {/*        <p>Aperiam architecto cupiditate possimus quae similique sunt tenetur. Ab, est?</p>*/}
+          {/*    </div>*/}
+          {/*    <div className="card"><img src="src/assets/images/7.jpg" alt="Alt"/>*/}
+          {/*        <h3>Dolorum esse, natus!</h3>*/}
+          {/*        <p>At beatae magnam odit perferendis quidem quisquam recusandae similique tempora.</p>*/}
+          {/*    </div>*/}
+          {/*    <div className="card"><img src="src/assets/images/8.jpg" alt="Alt"/>*/}
+          {/*        <h3>Ad, corporis, quod.</h3>*/}
+          {/*        <p>Blanditiis eos id qui reprehenderit temporibus tenetur vitae! Explicabo, praesentium.</p>*/}
+          {/*    </div>*/}
+
+          </header>
+
+          <div className="othercontent">
+              <p>©Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+      </div>
+  )
+}
+
+export default App
